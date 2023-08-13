ee852eee8f80c58f8bc358b2a312e2ab2435199a
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2023-08-13 21:43:23 +0530

    update api in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Weather_app/"
+  base: "/Weather_app/",
+  define: {
+    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+  },
 })
