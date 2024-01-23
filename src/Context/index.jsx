7247e14024429c29c9fbd6b8012ddde94fcc4e82
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2024-01-23 23:49:25 +0530

    optimizing api keys

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,7 +2,9 @@ import { useContext, createContext, useState, useEffect } from "react";
 import axios from 'axios'
 import { toast } from "react-toastify";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_KEYS = [import.meta.env.VITE_API_KEY_1, import.meta.env.VITE_API_KEY_2, import.meta.env.VITE_API_KEY_3];
+
 const StateContext = createContext();
 let flag =true;
 export const StateContextProvider = ({ children }) => {
@@ -12,6 +14,7 @@ export const StateContextProvider = ({ children }) => {
     const [thisLocation, setLocation] = useState('')
     const [timeZone, setTimeZone] = useState('Asia/Kolkata')
     const [isLoading, setIsLoading] = useState(true)
+    const [currentApiKeyIndex, setCurrentApiKeyIndex] = useState(0)
 
     // fetch api
     const fetchWeather = async () => {
@@ -26,7 +29,7 @@ export const StateContextProvider = ({ children }) => {
                 shortColumnNames: 0,
             },
             headers: {
-                'X-RapidAPI-Key': API_KEY,
+                'X-RapidAPI-Key': API_KEYS[currentApiKeyIndex],
                 'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
             }
             
@@ -34,7 +37,6 @@ export const StateContextProvider = ({ children }) => {
 
         try {
             const response = await axios.request(options);
-            // console.log(response.data)
             const thisData = Object.values(response.data.locations)[0]
             setTimeZone(thisData.tz)
             setLocation(thisData.address)
@@ -42,20 +44,28 @@ export const StateContextProvider = ({ children }) => {
             setWeather(thisData.values[0])
             flag=true;
             setIsLoading(false);
-        } catch (e) {
+        } catch (error) {
 
-            toast.error('This place (' +place+ ') does not exist', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme:"colored",
-                });
-                flag=false;
-                setIsLoading(true);
+            if(error.response && error.response.status === 429)
+            {
+                setCurrentApiKeyIndex((currentApiKeyIndex + 1) % API_KEYS.length);
+                fetchWeather();
+            }
+            else{
+                toast.error('This place (' +place+ ') does not exist', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme:"colored",
+                    });
+                    flag=false;
+                    setIsLoading(true);
+            }
+            
         }
 
         if(flag){
